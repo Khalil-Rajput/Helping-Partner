fe131f982b1c1d6e2d17c818f833c039737f9b46
Khalil-Rajput <167064059+Khalil-Rajput@users.noreply.github.com>
2025-10-16 12:54:33 +0200

    Add files via upload

---
+++ concat.bat
@@ -0,0 +1,46 @@
+@echo off
+echo // App.js > all_code.txt
+type App.js >> all_code.txt
+echo. >> all_code.txt
+echo // index.js >> all_code.txt
+type index.js >> all_code.txt
+echo. >> all_code.txt
+echo // app.json >> all_code.txt
+type app.json >> all_code.txt
+echo. >> all_code.txt
+echo // package.json >> all_code.txt
+type package.json >> all_code.txt
+echo. >> all_code.txt
+echo // TODO.md >> all_code.txt
+type TODO.md >> all_code.txt
+echo. >> all_code.txt
+echo // context/AuthContext.js >> all_code.txt
+type context\AuthContext.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/BidScreen.js >> all_code.txt
+type screens\BidScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/HomeScreen.js >> all_code.txt
+type screens\HomeScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/LoginScreen.js >> all_code.txt
+type screens\LoginScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/ProfileScreen.js >> all_code.txt
+type screens\ProfileScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/RegisterScreen.js >> all_code.txt
+type screens\RegisterScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/ServiceRequestScreen.js >> all_code.txt
+type screens\ServiceRequestScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/UserDashboardScreen.js >> all_code.txt
+type screens\UserDashboardScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // screens/WorkerDashboardScreen.js >> all_code.txt
+type screens\WorkerDashboardScreen.js >> all_code.txt
+echo. >> all_code.txt
+echo // utils/mocks.js >> all_code.txt
+type utils\mocks.js >> all_code.txt
+echo. >> all_code.txt
